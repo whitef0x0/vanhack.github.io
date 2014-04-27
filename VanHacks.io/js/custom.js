f878f2cffa342c714add92e39a48ec3b1470d815
David Baldwin <david@polyaic.com>
2014-04-27 05:07:55 -0700

    first commit

---
+++ VanHacks.io/js/custom.js
@@ -0,0 +1,57 @@
+  $(function() {
+    $( "#accordion" ).accordion();
+  });
+
+
+$(function()
+{
+    // bind 
+    $(window).resize(resize_site);
+
+    // load the website
+    setTimeout("load_site()", 0);
+
+    // setup typer
+    //$('[data-typer-targets]').typer();
+    $('#tlt').textillate({
+    minDisplayTime: 50, 
+    in: { effect: 'fadeInLeft', sync: true }, 
+    out: { effect: 'fadeOut', sync: true},
+    loop:true
+    });
+
+    $( "#accordion-resizer" ).resizable({
+      minHeight: 140,
+      minWidth: 200,
+      resize: function() {
+        $( "#accordion" ).accordion( "refresh" );
+      }
+    });
+    // registration toggle
+    /*
+    $("#pre-register").click(function()
+    {
+      $(this).fadeOut(100, function()
+      {
+        $("#register").fadeIn();
+      });
+    });
+    */ 
+                 
+});
+
+
+function load_site()
+{
+  resize_site();    
+  
+}
+
+// added in resize hacks
+function resize_site()
+{
+  $("header").height( $(window).height() * 0.60);
+  $("#teaser").height( $("#teaser h1").height() * 1.1);
+
+  //$("#logo-wrapper").offset({top : ($("header").height() - $("#logo-wrapper").height()) / 4});
+}
